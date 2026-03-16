642cb5b996afd88c4fa060cd2c351a9d4e11883d
marcoshideyuki <hideyukimarcos@gmail.com>
2026-03-16 15:44:15 -0300

    Elaboração do login

--- src/app.js
+++ src/app.js
@@ -31,5 +31,22 @@ async function cadastrar(){
 
         )
     }
+    window.location = "login.html"
     
+}
+
+async function entrar() {
+    const email = iptEmail.value
+    const senha = iptSenha.value
+    const resposta = await fetch("http://localhost:3000/usuarios")
+    const dados = await resposta.json()
+
+    for (let i = 0; i < dados.length; i++){
+        if (dados[i].email == email && dados[i].senha == senha){
+            window.location = "index.html"
+            console.log(dados[i])
+            return
+        }
+    }
+    window.alert("Verifique seus dados.")
 }
